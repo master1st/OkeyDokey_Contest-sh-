4fd573bb1808355586e3c3646fff4cede6713812
kimyeonchul <yeonchul0807@nate.com>
2023-05-28 21:53:58 +0900

    redux 적용

---
+++ OkeyDokeyContest/src/components/datas.js
@@ -0,0 +1,28 @@
+export const coffeeDatas = [
+  {
+    name: '아메리카노',
+    price: '4500',
+    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+  },
+  {
+    name: '아메리카노',
+    price: '4500',
+    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+  },
+  {
+    name: '아메리카노',
+    price: '4500',
+    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+  },
+  {
+    name: '아메리카노',
+    price: '4500',
+    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+  },
+];
+
+export const coffeeData = {
+  title: '아메리카노',
+  price: '4500',
+  imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+};

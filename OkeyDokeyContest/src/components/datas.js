a58cb8946ecddcc3b565a34f74407c21e50f3510
kimyeonchul <yeonchul0807@nate.com>
2023-05-31 23:57:15 +0900

    totalprice 수정

--- OkeyDokeyContest/src/components/datas.js
+++ OkeyDokeyContest/src/components/datas.js
@@ -1,56 +1,87 @@
 export const coffeeDatas = [
   {
-    id: 0,
-    name: '아메리카노',
-    price: '4500',
-    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+    name: '커피',
+    menues: [
+      {
+        id: 0,
+        name: '카페라떼',
+        price: '4500',
+        imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+      },
+      {
+        id: 1,
+        name: '바닐라라뗴',
+        price: '5500',
+        imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+      },
+    ],
   },
   {
-    id: 1,
-    name: '아메리카노',
-    price: '4500',
-    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+    name: '논커피',
+    menues: [
+      {
+        id: 2,
+        name: '아이스티',
+        price: '4500',
+        imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+      },
+      {
+        id: 3,
+        name: '아이스티',
+        price: '5500',
+        imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+      },
+    ],
   },
   {
-    id: 2,
-    name: '아메리카노',
-    price: '4500',
-    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+    name: '에이드',
+    menues: [
+      {
+        id: 4,
+        name: '체리콕',
+        price: '4500',
+        imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+      },
+      {
+        id: 5,
+        name: '청포도에이드',
+        price: '5500',
+        imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+      },
+    ],
   },
   {
-    id: 3,
-    name: '아메리카노',
-    price: '4500',
-    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+    name: '스무디',
+    menues: [
+      {
+        id: 6,
+        name: '딸기스무디',
+        price: '4500',
+        imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+      },
+      {
+        id: 7,
+        name: '딸기요거트스무디',
+        price: '5500',
+        imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+      },
+    ],
   },
   {
-    id: 4,
-    name: '아메리카노',
-    price: '4500',
-    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
-  },
-  {
-    id: 5,
-    name: '아메리카노',
-    price: '4500',
-    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
-  },
-  {
-    id: 6,
-    name: '아메리카노',
-    price: '4500',
-    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
-  },
-  {
-    id: 7,
-    name: '아메리카노',
-    price: '4500',
-    imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+    name: '티',
+    menues: [
+      {
+        id: 8,
+        name: '얼그레이',
+        price: '4500',
+        imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+      },
+      {
+        id: 9,
+        name: '히비스커스',
+        price: '5500',
+        imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
+      },
+    ],
   },
 ];
-
-export const coffeeData = {
-  title: '아메리카노',
-  price: '4500',
-  imgsrc: require('OkeyDokeyContest/assets/images/coffee.png'),
-};

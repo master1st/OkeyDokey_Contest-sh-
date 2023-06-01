8f3cdddf37b2fd82bad1afe0b03db8518e8ff092
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-06-01 17:24:42 +0900

    axios post

--- OkeyDokeyContest/src/API/api.js
+++ OkeyDokeyContest/src/API/api.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'HTTP-X-CSRFTOKEN';
+
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
-    headers : {
-        "Content-Type": "application/json"
-    },
-    withCredentials: true,
+  baseURL: 'http://13.125.232.138',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
 });
 
 export default API;

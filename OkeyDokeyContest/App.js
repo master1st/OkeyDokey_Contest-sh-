693e7ba94cbb84c541c45b6b2c06427804a666de
kimyeonchul <yeonchul0807@nate.com>
2023-08-27 13:36:49 +0900

    테스트코드 커밋하지 마라

--- OkeyDokeyContest/App.js
+++ OkeyDokeyContest/App.js
@@ -18,7 +18,7 @@ import Welcome from './src/screens/Welcome';
 import Identify from './src/screens/Identify';
 import Favorites from './src/screens/Favorites';
 import WebviewContainer from './src/pages/WebviewContainer';
-import FaceRecognition from './src/pages/FaceRecognition';
+import FaceRecognition from './src/pages/Facerecognition';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
@@ -27,9 +27,9 @@ const App = () => {
       <Provider store={store}>
         <Stack.Navigator screenOptions={{headerShown: false}}>
           {/* <Stack.Screen name="MyWebViewScreen" component={MyWebViewScreen} /> */}
-          {/* <Stack.Screen name="Welcome" component={Welcome} /> */}
+          <Stack.Screen name="Welcome" component={Welcome} />
           <Stack.Screen name="FaceRecognition" component={FaceRecognition} />
-           <Stack.Screen name="Identify" component={Identify} />
+          <Stack.Screen name="Identify" component={Identify} />
           <Stack.Screen name="WebviewContainer" component={WebviewContainer} />
           <Stack.Screen name="favorites" component={Favorites} />
           <Stack.Screen name="Home" component={Home} />
@@ -40,7 +40,7 @@ const App = () => {
           <Stack.Screen name="ShoppingBasket" component={ShoppingBasket} />
           <Stack.Screen name="OrderNum" component={OrderNum} />
           <Stack.Screen name="Payment" component={Payment} />
-          <Stack.Screen name="InputPhoneNum" component={InputPhoneNum} /> 
+          <Stack.Screen name="InputPhoneNum" component={InputPhoneNum} />
         </Stack.Navigator>
       </Provider>
     </NavigationContainer>
